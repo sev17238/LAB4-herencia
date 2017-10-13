9a37f6834b676ecbff2bd9c5272f00ed2e5ab873
SDiego <SDiego@SDiego-PC>
2017-10-13 08:46:53 -0600

    Agregado del metodo m2 y ToString() en la clase Alarma, parte del ejercicio 2.

--- LAB4 HERENCIA/src/AlarmaMain.java
+++ LAB4 HERENCIA/src/AlarmaMain.java
@@ -13,10 +13,9 @@
 
 public class AlarmaMain {
 	public static void main(String[] args) {
-		Alarma alarmaIncendio = new Alarma();
-		alarmaIncendio.m1();                  // emergencia 1
-		alarmaIncendio.m2();                  // alarma 1 / timbre 1
-		System.out.println(alarmaIncendio);   // emergencia Ring…Ring…Ring…Ring
-	}
-        
+		Alarma miReloj = new Alarma();
+                System.out.println(miReloj);   // emergencia Ring…Ring…Ring…Ring
+		miReloj.m1();                  // emergencia 1
+		miReloj.m2();                  // alarma 1 / timbre 1		
+	}        
 }
